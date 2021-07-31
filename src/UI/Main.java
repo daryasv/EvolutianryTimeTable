b618929c02aee2d63d1fa1c98661933375a0785b
daryasvirsky <svirskydarya@gmail.com>
2021-07-31 14:30:41 +0300

    add xml

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,8 +1,29 @@
 package UI;
 
+import schema.models.ETTDescriptor;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
+
 public class Main {
+    public static final String FILE_NAME = "src/resources/EX1-small.xml";
+
     public static void main(String[] args) {
-        ProgramManager.manageProgram();
+        //ProgramManager.manageProgram();
+
+        try {
+            File file = new File(FILE_NAME);
+            JAXBContext jaxbContext = JAXBContext.newInstance(ETTDescriptor.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            ETTDescriptor descriptor = (ETTDescriptor) jaxbUnmarshaller.unmarshal(file);
+            boolean a = true;
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 

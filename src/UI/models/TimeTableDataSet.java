df3f51a92eba2b9d63407ef98a344f8d683771c5
dar <dareini1995@gmail.com>
2021-08-06 16:41:24 +0300

    time table data set updates

--- src/UI/models/TimeTableDataSet.java
+++ src/UI/models/TimeTableDataSet.java
@@ -8,6 +8,9 @@ import engine.models.IRule;
 import engine.models.Solution;
 import schema.models.ETTDescriptor;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import UI.models.evolution.Mutation;
 
 
 public class TimeTableDataSet implements EvolutionDataSet<Lesson> {
@@ -19,6 +22,59 @@ public class TimeTableDataSet implements EvolutionDataSet<Lesson> {
         this.timeTableMembers = new TimeTableMembers(descriptor.getETTTimeTable());
         this.evolutionConfig = new EvolutionConfig(descriptor.getETTEvolutionEngine());
     }
+
+    public  void runMutation(Solution <Lesson> child){
+        List<Mutation> mutations = evolutionConfig.getMutations();
+        double probability;
+        int maxTupples;
+        char component;
+        for(int i=0; i<mutations.size();i++){
+            probability=mutations.get(i).getProbability();
+            maxTupples=mutations.get(i).getMaxTupples();
+            component=mutations.get(i).getComponent();
+            double randomNum = ThreadLocalRandom.current().nextDouble(0.0, 100.0);
+
+
+            if(mutations.get(i).getName().equals(Mutation.MutationOperators.FLIP_OPERATOR.getOperatorName())){
+                if(randomNum< probability)
+                    runFlippingMutation(child,maxTupples, component);
+            }
+        }
+    }
+
+    public static void runFlippingMutation(Solution <Lesson> child, int maxTuples, char component){
+        Random rand = new Random();
+        int randomTuplesNum = rand.nextInt();
+        for(int i=0; i<randomTuplesNum; i ++){
+            if (i>maxTuples)
+                break;
+            else{
+                int tupleIndex = rand.nextInt(child.getList().size());
+                changeComponent(child.getList().get(tupleIndex), component);
+            }
+        }
+    }
+
+    public static void changeComponent(Lesson lesson, char component){
+        int val=0;
+        if(component=='C'){
+            lesson.setClassId(val);
+        }
+        else if(component=='T'){
+            lesson.setTeacherId(val);
+        }
+        else if(component=='D'){
+            lesson.setDay(val);
+        }
+        else if(component=='H'){
+            lesson.setHour(val);
+        }
+        else if(component=='S'){
+            lesson.setSubjectId(val);
+        }
+    }
+
+
 
     //TODO
     @Override
